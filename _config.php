a1fc2e90317d98a7fcf2928ddec27e08dc6abf3c
yozakuraz <145245611+yozakuraz@users.noreply.github.com>
2023-11-14 05:10:10 +0700

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,34 @@
+<?php 
+
+$websiteTitle = "AnimeZia"; // Your Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = "https://cdnzia.pages.dev/images/logo.webp"; // Your Logo
+$websiteLogo2 = "https://cdnzia.pages.dev/images/logo2.webp";
+$contactEmail = ""; // Contact Email, put your own.
+
+
+//Donate 
+$donate = "https://www.ko-fi.com/warlords";
+
+// Socials 
+$telegram = "https://telegram.me/anime_arsenal"; // telegram
+$discord = "#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+// *** IMPORTANT ***  
+// remove slash - "/" from links if present.  
+
+$disqus = "https://watchzia.disqus.com"; // Disqus make your own
+
+$cdn = "https://cdnzia.pages.dev"; // cdn repo https://github.com/warlordsnet/cdnzia
+
+// API URL
+$api =""; // api repo https://github.com/warlordsnet/zia-api
+
+$imgk = ""; // your imagekit id, make your own. instructions in readme.
+
+$ani = =""; // api repo https://github.com/warlordsnet/consu-api
+
+$banner = "https://cdnzia.pages.dev/images/banner.webp";  //Banner use your own
+?>
